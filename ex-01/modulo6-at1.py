0858d0d24397fea174a987d50050daab0d2494f2
diego-salves <diegosantos57@outlook.com.br>
2024-04-10 21:05:19 -0300

    correção aula

---
+++ ex-01/modulo6-at1.py
@@ -0,0 +1,51 @@
+"""
+EXERCÍCIOS
+
+1. Faça um programa que leia um arquivo texto contendo uma lista de endereços IP e gere um outro arquivo, contendo um relatório dos endereços IP válidos e inválidos.
+
+O arquivo de entrada possui o seguinte formato:
+200.135.80.9
+192.168.1.1
+8.35.67.74
+257.34.2.5
+85.345.1.2
+1.2.3.4
+9.8.234.5
+192.168.0.256
+
+O arquivo de saída possui o seguinte formato:
+
+[Endereços válidos:]
+200.135.80.9
+192.168.1.1
+8.35.67.74
+1.2.3.4
+
+[Endereços inválidos:]
+257.34.2.5
+85.345.1.2
+9.8.234.5
+192.168.0.256
+
+A regra é que o intervalo de endereçamento de IP vai de 0.0.0.0 a 255.255.255.255.
+"""
+
+lista = ['200.135.80.9', '192.168.1.1', '8.35.67.74', '257.34.2.5', '85.345.1.2', '1.2.3.4', '9.8.234.5',
+         '192.168.0.256']
+
+
+def valida_ips(lista_de_ips):
+    ips_validos = []
+    ips_invalidos = []
+
+    for ip in lista_de_ips:
+        intervalo = ip.split('.')
+        if len(intervalo) == 4 and all(x.isdigit() and 0 <= int(x) < 256 for x in intervalo):
+            ips_validos.append(ip)
+        else:
+            ips_invalidos.append(ip)
+
+    return print("IPs válidos:", ips_validos), print("IPs Inválidos:", ips_invalidos)
+
+
+valida_ips(lista)
